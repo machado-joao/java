98eeda702f1c6e026d43173553bd2ecf1386a7e9
machado-joao <joaolucas.cm@hotmail.com>
2022-05-15 12:41:51 -0300

    Added a new file and fixed indentation

---
+++ LP/Lista 6/Ex20_v2.java
@@ -0,0 +1,22 @@
+public class Ex20_v2 {
+
+    public static void main(String[] args) {
+        int maior = 0, menor = 0;
+        for(int i = 1; i <= 100; i++) {
+            int valor = (int) (Math.random() * 100) + 1;
+            if(i == 1) {
+                maior = valor;
+                menor = valor;
+            }
+            if(valor > maior) {
+                maior = valor;
+            }
+            if(valor < menor) {
+                menor = valor;
+            }
+        }
+        System.out.println("Maior valor encontrado: " + maior);
+        System.out.println("Menor valor encontrado: " + menor);
+    }
+
+}
